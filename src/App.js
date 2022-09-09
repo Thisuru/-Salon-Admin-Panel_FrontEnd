ed9d588bd55dd3c3cec1408794fb59de25bc0c54
Thisuru Deesan <thisuru.d@eyepax.com>
2022-09-10 00:02:41 +0530

    create new page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
-import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Calendar from "./pages/calendar/Calendar";
+import { userInputs, userUpdateInputs } from "./formSource";
 
 
 function App() {
@@ -13,12 +14,17 @@ function App() {
         <Routes>
           <Route path="/"> 
             <Route index element={<Home />} />
+
             <Route path="login" element={<Login/>} />
+
             <Route path="users">
               <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New/>} />
+              <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+              <Route path="edit/:id" element={<New inputs={userUpdateInputs} title="Edit User" />} />
+
             </Route>
+
+            <Route path="calendar" element={<Calendar/>} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/formSource.js
@@ -0,0 +1,100 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "John Doe",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "john_doe@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+1 234 567 89",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Elton St. 216 NewYork",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "USA",
+    },
+  ];
+
+  export const userUpdateInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe",
+      value:"john_doe"
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "John Doe",
+      value:"John Doe"
+
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "john_doe@gmail.com",
+      value:"john_doe@gmail.com"
+
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+1 234 567 89",
+      value: "+1 234 567 89",
+
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+      value: "password",
+
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Elton St. 216 NewYork",
+      value: "Elton St. 216 NewYork",
+
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "USA",
+      value: "USA",
+
+    },
+  ];
