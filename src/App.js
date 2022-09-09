eed38649986459aab1616251ecd64107749803a1
Thisuru Deesan <thisuru.d@eyepax.com>
2022-09-09 14:03:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Admin Panel</h1>
+    </div>
+  );
+}
+
+export default App;
