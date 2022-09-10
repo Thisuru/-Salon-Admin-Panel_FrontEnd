00abc4c393a1d42a5aa65f24244e6de1fe29d31b
Thisuru Deesan <thisuru.d@eyepax.com>
2022-09-10 14:34:24 +0530

    add new Client fields changed in formSource.js

--- src/formSource.js
+++ src/formSource.js
@@ -1,13 +1,13 @@
 export const userInputs = [
     {
       id: 1,
-      label: "Username",
+      label: "First Name",
       type: "text",
       placeholder: "john_doe",
     },
     {
       id: 2,
-      label: "Name and surname",
+      label: "Last Name",
       type: "text",
       placeholder: "John Doe",
     },
@@ -23,36 +23,36 @@ export const userInputs = [
       type: "text",
       placeholder: "+1 234 567 89",
     },
-    {
-      id: 5,
-      label: "Password",
-      type: "password",
-    },
-    {
-      id: 6,
-      label: "Address",
-      type: "text",
-      placeholder: "Elton St. 216 NewYork",
-    },
-    {
-      id: 7,
-      label: "Country",
-      type: "text",
-      placeholder: "USA",
-    },
+    // {
+    //   id: 5,
+    //   label: "Password",
+    //   type: "password",
+    // },
+    // {
+    //   id: 6,
+    //   label: "Address",
+    //   type: "text",
+    //   placeholder: "Elton St. 216 NewYork",
+    // },
+    // {
+    //   id: 7,
+    //   label: "Country",
+    //   type: "text",
+    //   placeholder: "USA",
+    // },
   ];
 
   export const userUpdateInputs = [
     {
       id: 1,
-      label: "Username",
+      label: "First Name",
       type: "text",
       placeholder: "john_doe",
       value:"john_doe"
     },
     {
       id: 2,
-      label: "Name and surname",
+      label: "Last Name",
       type: "text",
       placeholder: "John Doe",
       value:"John Doe"
@@ -74,27 +74,27 @@ export const userInputs = [
       value: "+1 234 567 89",
 
     },
-    {
-      id: 5,
-      label: "Password",
-      type: "password",
-      value: "password",
+    // {
+    //   id: 5,
+    //   label: "Password",
+    //   type: "password",
+    //   value: "password",
 
-    },
-    {
-      id: 6,
-      label: "Address",
-      type: "text",
-      placeholder: "Elton St. 216 NewYork",
-      value: "Elton St. 216 NewYork",
+    // },
+    // {
+    //   id: 6,
+    //   label: "Address",
+    //   type: "text",
+    //   placeholder: "Elton St. 216 NewYork",
+    //   value: "Elton St. 216 NewYork",
 
-    },
-    {
-      id: 7,
-      label: "Country",
-      type: "text",
-      placeholder: "USA",
-      value: "USA",
+    // },
+    // {
+    //   id: 7,
+    //   label: "Country",
+    //   type: "text",
+    //   placeholder: "USA",
+    //   value: "USA",
 
-    },
+    // },
   ];
