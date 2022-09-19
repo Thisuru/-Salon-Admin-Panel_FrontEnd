b7c2afe78bb660973a6c14d42ec6cd7307a8b5d4
Thisuru Deesan <thisuru.d@eyepax.com>
2022-09-19 15:01:00 +0530

    login page styles added and Nav bar added to login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -12,6 +12,7 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
 
 
 const Login = () => {
@@ -45,65 +46,68 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Container maxWidth="sm">
-        <Grid
-          container
-          spacing={2}
-          direction="column"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Paper elelvation={2} sx={{ padding: 5 }}>
-            <form onSubmit={handleSubmit}>
-              <Grid container direction="column" spacing={2}>
-                <Grid item>
-                  <TextField
-                    type="text"
-                    fullWidth
-                    label="Enter your email"
-                    placeholder="Email Address"
-                    variant="outlined"
-                    required
-                    onChange={(e) => setValues({ ...values, username: e.target.value })}
-                  />
-                </Grid>
+    <div className="listContainer">
+      <Navbar/>
+      <div>
+        <Container maxWidth="sm">
+          <Grid
+            container
+            spacing={2}
+            direction="column"
+            justifyContent="center"
+            style={{ minHeight: "100vh" }}
+          >
+            <Paper elelvation={2} sx={{ padding: 5 }}>
+              <form onSubmit={handleSubmit}>
+                <Grid container direction="column" spacing={2}>
+                  <Grid item>
+                    <TextField
+                      type="text"
+                      fullWidth
+                      label="Enter your username"
+                      placeholder="Username"
+                      variant="outlined"
+                      required
+                      onChange={(e) => setValues({ ...values, username: e.target.value })}
+                    />
+                  </Grid>
 
-                <Grid item>
-                  <TextField
-                    type={values.showPass ? "text" : "password"}
-                    fullWidth
-                    label="Password"
-                    placeholder="Password"
-                    variant="outlined"
-                    required
-                    onChange={(e) => setValues({ ...values, password: e.target.value })}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            onClick={handlePassVisibilty}
-                            aria-label="toggle password"
-                            edge="end"
-                          >
-                            {values.showPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
+                  <Grid item>
+                    <TextField
+                      type={values.showPass ? "text" : "password"}
+                      fullWidth
+                      label="Password"
+                      placeholder="Password"
+                      variant="outlined"
+                      required
+                      onChange={(e) => setValues({ ...values, password: e.target.value })}
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              onClick={handlePassVisibilty}
+                              aria-label="toggle password"
+                              edge="end"
+                            >
+                              {values.showPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
 
-                <Grid item>
-                  <Button type="submit" fullWidth variant="contained">
-                    Sign In
-                  </Button>
+                  <Grid item>
+                    <Button style={{backgroundColor: '#e9205c'}} type="submit" fullWidth variant="contained">
+                      Sign In
+                    </Button>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </form>
-          </Paper>
-        </Grid>
-      </Container>
+              </form>
+            </Paper>
+          </Grid>
+        </Container>
+      </div>
     </div>
   );
 };
