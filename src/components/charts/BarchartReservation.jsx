10b572f25828b87fabb77c0d6687f0428343306f
Thisuru Deesan <thisuru.d@eyepax.com>
2022-10-06 16:31:35 +0530

    Barchart weekly plot stable point

--- src/components/charts/BarchartReservation.jsx
+++ src/components/charts/BarchartReservation.jsx
@@ -1,5 +1,9 @@
 import { Card, CardContent, Divider, Typography } from "@mui/material";
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   BarChart,
   Bar,
@@ -12,26 +16,68 @@ import {
 
 const data = [
   {
-    name: "Res 1",
+    name: "Stylist 1",
     uv: 4000,
-    pv: 2400,
+    pv: 60,
     amt: 2400
   },
   {
-    name: "Res 2",
+    name: "Stylist 2",
     uv: 3000,
-    pv: 1398,
+    pv: 60,
     amt: 2210
   },
   {
-    name: "Res 3",
+    name: "Stylist 3",
     uv: 2000,
-    pv: 9800,
+    pv: 105,
     amt: 2290
   }
 ];
 
 const BarchartReservation = () => {
+
+  const [barData, setBarData] = useState([]);
+  
+  useEffect(() => {
+    getWeeklyReservationData()
+  }, []);
+
+  async function getWeeklyReservationData() {
+    const response = await axios.get("http://localhost:5000/api/v1/stylists/getEachStylistTime");
+
+    if (response.status === 200) {
+      console.log("getWeeklyReservationData: ", response.data);
+      let getWeeklyReservationData = response.data
+      let barChartDataArray = [];
+
+      for (let i = 0; i < getWeeklyReservationData.length; i++) {
+        
+        let eachReservation = getWeeklyReservationData[i]
+        let start = new Date(eachReservation.startTime)
+        let end = new Date(eachReservation.endTime)
+        let minutesDifference = getTimeDifference(start, end);
+
+        eachReservation.WeeklyTimeInMins = minutesDifference;
+        eachReservation.name = "Sty " + i
+        barChartDataArray.push(eachReservation)
+      }
+      console.log("Final: ", barChartDataArray);
+      setBarData(barChartDataArray)
+      toast("Success! Client created successfully", { type: "success" });
+
+    } else {
+      toast(response.data.message, { type: "error" });
+    }
+  }
+
+  //get startTime and endTime difference
+  const getTimeDifference = (startDate, endDate) => {
+    const msInHour = 1000 * 60;
+
+    return Math.round(Math.abs(endDate - startDate) / msInHour);
+  }
+
   return (
     <Card>
       <CardContent>
@@ -43,7 +89,7 @@ const BarchartReservation = () => {
           <BarChart
             width={500}
             height={500}
-            data={data}
+            data={barData}
             margin={{
               top: 65,
               bottom: 15
@@ -54,7 +100,7 @@ const BarchartReservation = () => {
             <Tooltip />
             <Legend />
             <CartesianGrid strokeDasharray="3 3" />
-            <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
+            <Bar dataKey="WeeklyTimeInMins" fill="#8884d8" background={{ fill: "#eee" }} />
           </BarChart>
         </div>
       </CardContent>
